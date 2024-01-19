313a2d9bfa1cfb9c1d110895343e51db752a0bb3
Sharanya Thammi <sharanyachandu5a9@gmail.com>
2024-01-19 10:54:30 +0100

    auto save

---
+++ data.tf
@@ -0,0 +1,29 @@
+# Retrieves the information from the remote vpc state file
+data "terraform_remote_state" "vpc" {
+  backend  = "s3"
+  config = {
+    bucket = "b53-s3-bucket"
+    key    = "vpc/${var.ENV}/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+#Retrieves the information from the remote vpc alb file
+data "terraform_remote_state" "alb" {
+  backend  = "s3"
+  config = {
+    bucket = "b53-s3-bucket"
+    key    = "alb/${var.ENV}/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+£Retrieves the information from the remote db file
+data "terraform_remote_state" "db" {
+  backend  = "s3"
+  config = {
+    bucket = "b53-s3-bucket"
+    key    = "databases/${var.ENV}/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
